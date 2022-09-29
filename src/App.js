ca266dedd2f390b5121983d3bfadcbce58755f62
Yaroslav Magrelo <slava.magrelo@gmail.com>
2022-09-29 22:16:57 +0300

    added cart provider

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
+import { logDOM } from '@testing-library/react';
 
 const App = () => {
+  const me = {
+    name: 'Yar ',
+    talk() {
+      return console.log(`Hello I am ${this.name}`);
+    },
+  };
+
+  me.talk();
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
