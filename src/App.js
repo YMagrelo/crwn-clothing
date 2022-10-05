5090583c382eac90620b1fe2c4b8c164e6a74ccc
Yaroslav Magrelo <slava.magrelo@gmail.com>
2022-10-05 22:10:37 +0300

    added in our addToCart feature

--- src/App.js
+++ src/App.js
@@ -4,17 +4,8 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
-import { logDOM } from '@testing-library/react';
 
 const App = () => {
-  const me = {
-    name: 'Yar ',
-    talk() {
-      return console.log(`Hello I am ${this.name}`);
-    },
-  };
-
-  me.talk();
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -21,17 +21,32 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const totalSum = cartItems.reduce((acc, item) => {
+      return acc + item.quantity;
+    }, 0);
+    setCartCount(totalSum);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
